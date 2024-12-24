6d1933ba9a4ba480d39c3f66d7f7ac6f6f609205
apple <nikhiljain1203@gmail.com>
2024-12-24 22:27:06 +0530

    First Class

---
+++ src/main/java/com/example/gatewaynov24/GatewayNov24Application.java
@@ -0,0 +1,13 @@
+package com.example.gatewaynov24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayNov24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayNov24Application.class, args);
+    }
+
+}
